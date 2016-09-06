9b98f171cc978a49422e5f5e99a7d70f22e231d3
Travis Glenn Hansen <travisghansen@yahoo.com>
2016-09-06 16:10:22 -0600

    support multiple accountSids

--- html/send.php
+++ html/send.php
@@ -3,29 +3,32 @@
 //https://www.twilio.com/docs/api/rest/message
 
 //includes an array $config with account details etc
-$config_path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
-require_once($config_path);
-
-//per setup/instance
-$accountSid = $config['accountSid'];
-$authToken = $config['authToken'];;
-
-//generate hash for hacky auth
-$hash = sha1("${accountSid}:${authToken}");
+$config_path = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+require_once $config_path;
 
 //ensure hash is sane
-if($hash != $_GET['auth_hash']){
-	$response = "failure";
-	goto end;
+if ($config['token'] != $_REQUEST['token']) {
+    $response = 'failure';
+    goto end;
 }
 
 //per message
-$from = $_GET['from'];
-$to = $_GET['to'];
-$body = $_GET['text'];
+$from = $_REQUEST['from'];
+$to = $_REQUEST['to'];
+$body = $_REQUEST['text'];
+$accountSid = $_REQUEST['accountSid'];
+$authToken = $_REQUEST['authToken'];
 
-if(empty($from)){
-	$from = $config['defaultFrom'];
+if (empty($from)) {
+    $from = $config['defaultFrom'];
+}
+
+if (empty($accountSid)) {
+    $accountSid = $config['defaultAccountSid'];
+}
+
+if (empty($authToken)) {
+    $authToken = $config['defaultAuthToken'];
 }
 
 //not implemented
@@ -37,11 +40,10 @@ $url = "https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json";
 
 //setup POST params
 $params = array(
-	"From" => $from,
-	"To"   => $to,
-	"Body" => $body,
+    'From' => $from,
+    'To' => $to,
+    'Body' => $body,
 );
-
 
 $c = curl_init($url);
 curl_setopt($c, CURLOPT_POST, 1);
@@ -52,22 +54,19 @@ curl_setopt($c, CURLOPT_USERPWD, "${accountSid}:${authToken}");
 
 $page = curl_exec($c);
 
-if(curl_errno($c)){
-	$error = curl_error($c);
-	$response = "retry later";
-}else{
-	$data = json_decode($page, true);
-	if($data['status'] == "queued"){
-		$response = "ok";
-	}else{
-		$response = "failure";
-	}
+if (curl_errno($c)) {
+    $error = curl_error($c);
+    $response = 'retry later';
+} else {
+    $data = json_decode($page, true);
+    if ($data['status'] == 'queued') {
+        $response = 'ok';
+    } else {
+        $response = 'failure';
+    }
 }
 curl_close($c);
 
-
 end:
-header("Content-Type:text/plain");
+header('Content-Type:text/plain');
 echo $response;
-
-?>
